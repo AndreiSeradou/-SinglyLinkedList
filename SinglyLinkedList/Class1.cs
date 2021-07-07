dda3917394d37411777f212293b321602afacc1d
AndreiSeradou <andreiseradou@gmail.com>
2021-07-07 09:00:58 +0300

    Ready Task

---
+++ SinglyLinkedList/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    class SinglyLinkedList<T>
+    {
+        private class Node
+        {
+            public Node()
+            {
+
+            }
+            public Node nextNode;
+            public T value;
+        }
+        private Node topNode;
+
+        public SinglyLinkedList()
+        {
+            topNode = null;
+        }
+
+        public void PushBack(T value)
+        {
+            if (topNode == null)
+            {
+                Node firstTop= new Node();
+                firstTop.value = value;
+
+                firstTop.nextNode = null;
+                topNode = firstTop;
+
+                return;
+            }
+            Node newTop = new Node();
+            newTop.value = value;
+
+            newTop.nextNode= topNode;
+            topNode = newTop;
+        }
+        public void PopElement(T value)
+        {
+            Node currentNode = topNode;
+
+            if (currentNode.value.Equals(value))
+            {
+                topNode = currentNode.nextNode;
+                return;
+            }
+
+            while (currentNode.nextNode != null)
+            {
+                if (currentNode.nextNode.value.Equals(value))
+                {
+                    if (currentNode.nextNode != null)
+                    {
+                        currentNode.nextNode= currentNode.nextNode.nextNode;
+                        return;
+                    }
+                }
+                currentNode = currentNode.nextNode;
+            }
+        }
+        public bool Find(T value)
+        {
+            Node currentNode = topNode;
+
+            while (currentNode.nextNode != null)
+            {
+                if (currentNode.value.Equals(value))
+                {
+                    return true;
+                }
+                currentNode = currentNode.nextNode;
+            }
+            if (currentNode.value.Equals(value))
+                return true;
+            return false;
+        }
+        public T GetFrontElement()
+        {
+            Node currentElement = topNode;
+
+            while (currentElement.nextNode != null)
+            {
+                currentElement = currentElement.nextNode;
+            }
+            return currentElement.value;
+        }
+
+        public void PrintList()
+        {
+            if (topNode == null)
+            {
+                string message = "Its empty\n";
+                throw new ArgumentNullException(message);
+            }
+            Node current_element = topNode;
+
+            while (current_element != null)
+            {
+                Console.WriteLine(current_element.value + " ");
+                current_element = current_element.nextNode;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList<int> unidirectional_list = new SinglyLinkedList<int>();
+            unidirectional_list.PushBack(2);
+            unidirectional_list.PushBack(3);
+            unidirectional_list.PushBack(4);
+            unidirectional_list.PushBack(5);
+            Console.WriteLine(unidirectional_list.Find(3));
+            Console.WriteLine(unidirectional_list.Find(2));
+            Console.WriteLine(unidirectional_list.Find(5));
+            Console.WriteLine(unidirectional_list.Find(0));
+            unidirectional_list.PrintList();
+            unidirectional_list.PopElement(3);
+            unidirectional_list.PrintList();
+            unidirectional_list.PopElement(2);
+            unidirectional_list.PrintList();
+            unidirectional_list.PopElement(4);
+            unidirectional_list.PrintList();
+
+        }
+    }
+}
